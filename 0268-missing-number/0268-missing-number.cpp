22a551fecc0c54efcefe0ac9935a77ee5dfa8431
Priya6423 <142709607+Priya6423@users.noreply.github.com>
2026-02-01 21:47:36 +0530

    Time: 0 ms (100%), Space: 22.6 MB (5.07%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+    int n=nums.size();
+    vector<int> ans(n+1,0);
+    for(int i=0;i<n;i++){
+    ans[nums[i]]++;
+    }
+    for(int i=0;i<ans.size();i++){
+    if(ans[i]==0){
+    return i;
+    }
+    }
+    return -1;
+    }
+};
