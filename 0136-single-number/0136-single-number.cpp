2555a05dabb195440ceaee736d76fce996bb4710
Priya6423 <142709607+Priya6423@users.noreply.github.com>
2026-02-03 23:48:34 +0530

    Time: 0 ms (100%), Space: 20.7 MB (24.68%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,15 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-    unordered_map<int,int> mp;
-    for(int i=0;i<nums.size();i++){
-    mp[nums[i]]++;
-    }
-    for(auto it:mp){
-    if(it.second==1){
-    return it.first;
-    }
-    }  
-    return -1; 
+        int result = 0;
+        
+        for (int num : nums) {
+            result ^= num;  // XOR operation
+        }
+        
+        return result;
     }
 };
