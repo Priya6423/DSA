5b976454220345b1fb79500c02211f3c30e329a4
Priya6423 <142709607+Priya6423@users.noreply.github.com>
2026-02-08 14:19:18 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.32%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+    if(n<3) return n;
+    int prev1=1,prev2=2,curr=0;
+    for(int i=3;i<=n;i++){
+    curr=prev1+prev2;
+    prev1=prev2;
+    prev2=curr;
+    }
+    return curr;
+    }
+};
