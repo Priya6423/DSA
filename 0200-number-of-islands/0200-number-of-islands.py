0b07275fed8cfa8d084f17dc20ca877477e7b7df
Priya6423 <142709607+Priya6423@users.noreply.github.com>
2026-06-06 13:00:53 +0530

    Time: 243 ms (61.06%), Space: 21.5 MB (73.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        count = 0
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c]=='1':
+        rows,cols=len(grid),len(grid[0])
+        queue=deque()
+        count=0
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col]=='1':
                     count+=1
-                    grid[r][c]='0'
-                    queue=deque([(r,c)])
-                    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+                    grid[row][col]='0'
+                    queue.append((row,col))
+                    directions=[(-1,0),(1,0),(0,-1),(0,1)]
                     while queue:
-                        cur_r,cur_c=queue.popleft()
-                        for dr, dc in directions:
-                            new_r, new_c = cur_r + dr, cur_c + dc
-                            if 0<=new_r<rows and 0<=new_c<cols:
-                                if grid[new_r][new_c]=='1':
-                                    grid[new_r][new_c]='0'
-                                    queue.append((new_r,new_c))
+                        curr,curc=queue.pop()
+                        for dr,dc in directions:
+                            newr,newc=curr+dr,curc+dc
+                            if 0<=newr<rows and 0<=newc<cols:
+                                if grid[newr][newc]=='1':
+                                    queue.append((newr,newc))
+                                    grid[newr][newc]='0'
         return count
-        
 
-    
+
+
+
+
+        
