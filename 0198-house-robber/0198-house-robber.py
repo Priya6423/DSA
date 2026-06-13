2e51973f235285f750d892c33f920eb57a7f5c27
Priya6423 <142709607+Priya6423@users.noreply.github.com>
2026-06-13 17:48:35 +0530

    Time: 0 ms (100%), Space: 19.3 MB (24.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,11 +3,11 @@ class Solution:
         n=len(nums)
         best=[0]*n
         if n==1 :return nums[0]
-        best[0]=nums[0]
-        best[1]=max(nums[0],nums[1])
+        prev1=nums[0]
+        prev2=max(nums[0],nums[1])
         for i in range(2,n):
-            best[i]=max(best[i-1],nums[i]+best[i-2])
-        return best[n-1]
+            prev1,prev2=prev2,max(prev2,nums[i]+prev1)
+        return prev2
         
 
         
