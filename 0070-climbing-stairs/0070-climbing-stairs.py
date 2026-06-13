4f01817d9bf0cc7fbc4b9274f3c0c8cf519baa9c
Priya6423 <142709607+Priya6423@users.noreply.github.com>
2026-06-13 14:39:41 +0530

    Time: 0 ms (100%), Space: 19.3 MB (53.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        def climb(n,dp):
-            if n<=2:
-                return n
-            if dp[n]!=-1:
-                return dp[n]
-            else:
-                dp[n]= climb(n-1,dp)+climb(n-2,dp)
-                return dp[n]
-        m=n+1
-        dp=[-1]*m
-        ans=climb(n,dp)
-        return ans
+        prev1=0
+        prev2=1
+        if n<=3: return n
+        while n:
+            prev1,prev2=prev2,prev1+prev2
+            n-=1
+        return prev2
 
         
