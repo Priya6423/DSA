0ff358f18bf05f5652c6cf3ff6a6f1b422280809
Priya6423 <142709607+Priya6423@users.noreply.github.com>
2026-07-06 23:05:55 +0530

    Time: 3 ms (77.5%), Space: 19.6 MB (80.88%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.py
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -1,17 +1,13 @@
 class Solution:
-    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
-        count=len(intervals)
-        covered=[False]*count
-        for i in range(len(intervals)):
-            for j in range(i+1,len(intervals)):
-                if intervals[i][0]>=intervals[j][0] and intervals[i][1]<=intervals[j][1]:
-                    covered[i]=True
-                    break
-                elif intervals[i][0]<=intervals[j][0] and intervals[i][1]>=intervals[j][1]:
-                    covered[j]=True
-        for i in covered:
-            if i:
-                count-=1
+    def removeCoveredIntervals(self, intervals):
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+        count = 0
+        max_end = 0
+        for start, end in intervals:
+            if end>max_end:
+                count+=1
+                max_end=end           
+                        
+            
         return count
-
         
